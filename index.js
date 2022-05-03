f7c6e5c7134fb8350a055b17fa1d1b87bf55244d
Sparshak20 <76407421+Sparshak20@users.noreply.github.com>
2022-05-03 14:36:33 +0530

    Day 1

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Hello') /*String */
+console.log(4*3) /* Int*/ 
+console.log([3,2,21,3]) /*Array*/
+/* JS Object */
+console.log({Harry:'this',marks:22})
+/* To show a table */
+console.table({Harry:'this',marks:22})
+/* To show a Warning */
+console.warn('This is a warning')
+/* to clear the console */
+console.clear()
